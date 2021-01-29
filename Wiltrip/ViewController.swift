e30dcef7bae6f4f54e731edd87c01c8b52ea6e1d
mydm <ikhwanagus01@gmail.com>
2021-01-29 08:30:26 +0700

    Initial Commit

---
+++ Wiltrip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wiltrip
+//
+//  Created by Agustian DM on 29/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
